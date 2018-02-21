db2105ef11a6d4e62f3aae4c470b43579064cf30
yogurt1989 <mauropappaterra@gmail.com>
2018-02-21 20:44:58 +0100

    added code comments in all scripts

--- js/view/view_bartender.js
+++ js/view/view_bartender.js
@@ -7,7 +7,11 @@
 *  The indexes and their corresponding languages: [0] English , [1] Swedish, [2] Italian, [3] Spanish, [4] Pirate
 *  Follow this template:
 *
-*  var redundant_name = ["ENGLISH","SWEDISH","ITALIAN","SPANISH","PIRATE"]
+*  var redundant_name = ["ENGLISH",
+*                        "SWEDISH",
+*                        "ITALIAN",
+*                        "SPANISH",
+*                        "PIRATE"]
 *
 *  Do not skip any indexes! Locate HTML object add id="unique_id" on the HTML file.
 *  Then update translate() method on the corresponding js script page!
--- js/view/view_management.js
+++ js/view/view_management.js
@@ -7,16 +7,24 @@
 *  The indexes and their corresponding languages: [0] English , [1] Swedish, [2] Italian, [3] Spanish, [4] Pirate
 *  Follow this template:
 *
-*  var redundant_name = ["ENGLISH","SWEDISH","ITALIAN","SPANISH","PIRATE"]
+*  var redundant_name = ["ENGLISH",
+*                        "SWEDISH",
+*                        "ITALIAN",
+*                        "SPANISH",
+*                        "PIRATE"]
 *
 *  Do not skip any indexes! Locate HTML object add id="unique_id" on the HTML file.
 *  Then update translate() method on the corresponding js script page!
 */
 
 var title = ["Management View",
-    "Arbetledarsidan",
-    "Vista Gestione",
-    "Vista Administración",
-    "Kin' o' th' Pirates"]
+             "Arbetledarsidan",
+             "Vista Gestione",
+             "Vista Administración",
+             "Kin' o' th' Pirates"]
 
-var stock_order = ["Stock Order","SWEDISH","Ordine","Perdido","PIRATE"]
+var stock_order = ["Stock Order",
+                   "SWEDISH",
+                   "Ordine",
+                   "Perdido",
+                    "PIRATE"]
