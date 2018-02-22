5872c2bab24cc94bf681cc8c3d035990fa4e936e
yogurt1989 <mauropappaterra@gmail.com>
2018-02-22 21:36:18 +0100

    redesigned bartender view

--- js/bartender.js
+++ js/bartender.js
@@ -8,6 +8,21 @@
 * Methods translate() and responsive() are unique for each individual page.
 */
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
 function translate (index) {
     $("#page_title").text(page_title[index]);
     $("#title").text(title[index]);
