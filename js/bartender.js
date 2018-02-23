7d61f34a2359da12b4d10339a224f7c3cc7d3b5a
yogurt1989 <mauropappaterra@gmail.com>
2018-02-23 15:15:15 +0100

    added cursors for better haptic effects

--- js/bartender.js
+++ js/bartender.js
@@ -14,6 +14,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
+        this.classList.toggle("zoom_out")
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight){
             panel.style.maxHeight = null;
