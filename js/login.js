8e7ea57ebd140ec997e9db901b1e873f340731d7
yaila <jenny_40@hotmail.com>
2018-03-13 22:31:41 +0100

    Implemented loginfunctionality

--- js/login.js
+++ js/login.js
@@ -1,6 +1,11 @@
+//const DB = require('../db/DB.js');
+//import {tmp} from '../db/DB';
+
+
 function translate (index) {
     $("#page_title").text(page_title[index]);
     $("#user_name").text(user_name[index]);
     $("#password").text(password[index]);
     $("#login").text(login[index]);
 }
+
