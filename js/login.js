6704f89f651e77aa31d417ba506e93556c1595de
yaila <jenny_4o@hotmail.com>
2018-03-15 12:20:51 +0100

    First draft of login page

---
+++ js/login.js
@@ -0,0 +1,6 @@
+function translate (index) {
+    $("#page_title").text(page_title[index]);
+    $("#user_name").text(user_name[index]);
+    $("#password").text(password[index]);
+    $("#login").text(login[index]);
+}
