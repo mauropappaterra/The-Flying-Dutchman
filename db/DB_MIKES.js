efb4d4c88105adcdcac0e121f32f9b8eb2f34cca
Mauro José Pappaterra <mauropappaterra@gmail.com>
2018-03-18 08:49:17 +0100

    Update DB_MIKES.js

--- db/DB_MIKES.js
+++ db/DB_MIKES.js
@@ -1,147 +1,151 @@
 var DB_MIKES = [
-        {
-            "article_id": "MIKE01",
-            "name": "mike's Robust Imperial Porter",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 8.0,
-            "sale_price": 50.0
-            },
-        {
-            "article_id": "MIKE02",
-            "name": "Taranaki IPA",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 350,
-            "alcohol_percentage": 7.0,
-            "sale_price": 65.0
-            },
-        {
-            "article_id": "MIKE03",
-            "name": "Vanilla Coffee Porter",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 500,
-            "alcohol_percentage": 8.0,
-            "sale_price": 200.0
-            },
-        {
-            "article_id": "MIKE04",
-            "name": "Chocolate Milk Stout",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 8.0,
-            "sale_price": 150.0
-            },
-        {
-            "article_id": "MIKE05",
-            "name": "Onemorepaleale",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 5.5,
-            "sale_price": 120.0
-            },
-        {
-            "article_id": "MIKE06",
-            "name": "PilS-NAH",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 5.0,
-            "sale_price": 165.0
-            },
-        {
-            "article_id": "MIKE07",
-            "name": "Longline Lager",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 5.0,
-            "sale_price": 175.0
-            },
-        {
-            "article_id": "MIKE08",
-            "name": "Mild Ale",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 4.0,
-            "sale_price": 145.0
-            },
-        {
-            "article_id": "MIKE09",
-            "name": " Full Nelson IPA",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 7.0,
-            "sale_price": 145.0
-            },
-        {
-            "article_id": "MIKE10",
-            "name": " Second Coming IIPA",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 9.0,
-            "sale_price": 145.0
-            },
-        {
-            "article_id": "MIKE11",
-            "name": " Surf Spot X",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 5.7,
-            "sale_price": 115.0
-            },
-        {
-            "article_id": "MIKE12",
-            "name": "Boys N Girls",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 8.0,
-            "sale_price": 125.0
-            },
-        {
-            "article_id": "MIKE13",
-            "name": "Hopstock & 2 Smoking Barrels",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 5.0,
-            "sale_price": 125.0
-            },
-        {
-            "article_id": "MIKE14",
-            "name": "Damson in Distress",
-            "year": 2018,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 5.0,
-            "sale_price": 170.0
-            },
-        {
-            "article_id": "MIKE15",
-            "name": "Flight of the Falcon",
-            "year": 2010,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 5.7,
-            "sale_price": 185.0
-            },
-        {
-            "article_id": "MIKE16",
-            "name": "Leaky Boat",
-            "year": 2014,
-            "country": "New Zeland",
-            "volume_ml": 330,
-            "alcohol_percentage": 5.2,
-            "sale_price": 145.0
-             }
+    {
+        "artikelid": "MIKE01",
+        "namn": "Mike's Robust Imperial Porter",
+        "namn2": null,
+        "year": 2018,
+        "prisinklmoms": "50.00",
+        "volymiml": 330,
+        "varugrupp": "Beer",
+        "ursprunglandnamn": "New Zealand",
+        "producent": "Mike's Brewery",
+        "leverantor": "Kiwi Couriers AB",
+        "alkoholhalt": "8%"
+    }/*, // ALL BELOW SHOULD FOLLOW SAME FORMAT AS ABOVE!!! EXACTLY THE SAME (do not delete namn2 even if null!)
+    {
+        "article_id": "MIKE02",
+        "name": "Taranaki IPA",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 350,
+        "alcohol_percentage": 7.0,
+        "sale_price": 65.0
+    },
+    {
+        "article_id": "MIKE03",
+        "name": "Vanilla Coffee Porter",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 500,
+        "alcohol_percentage": 8.0,
+        "sale_price": 200.0
+    },
+    {
+        "article_id": "MIKE04",
+        "name": "Chocolate Milk Stout",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 8.0,
+        "sale_price": 150.0
+    },
+    {
+        "article_id": "MIKE05",
+        "name": "Onemorepaleale",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 5.5,
+        "sale_price": 120.0
+    },
+    {
+        "article_id": "MIKE06",
+        "name": "PilS-NAH",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 5.0,
+        "sale_price": 165.0
+    },
+    {
+        "article_id": "MIKE07",
+        "name": "Longline Lager",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 5.0,
+        "sale_price": 175.0
+    },
+    {
+        "article_id": "MIKE08",
+        "name": "Mild Ale",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 4.0,
+        "sale_price": 145.0
+    },
+    {
+        "article_id": "MIKE09",
+        "name": " Full Nelson IPA",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 7.0,
+        "sale_price": 145.0
+    },
+    {
+        "article_id": "MIKE10",
+        "name": " Second Coming IIPA",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 9.0,
+        "sale_price": 145.0
+    },
+    {
+        "article_id": "MIKE11",
+        "name": " Surf Spot X",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 5.7,
+        "sale_price": 115.0
+    },
+    {
+        "article_id": "MIKE12",
+        "name": "Boys N Girls",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 8.0,
+        "sale_price": 125.0
+    },
+    {
+        "article_id": "MIKE13",
+        "name": "Hopstock & 2 Smoking Barrels",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 5.0,
+        "sale_price": 125.0
+    },
+    {
+        "article_id": "MIKE14",
+        "name": "Damson in Distress",
+        "year": 2018,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 5.0,
+        "sale_price": 170.0
+    },
+    {
+        "article_id": "MIKE15",
+        "name": "Flight of the Falcon",
+        "year": 2010,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 5.7,
+        "sale_price": 185.0
+    },
+    {
+        "article_id": "MIKE16",
+        "name": "Leaky Boat",
+        "year": 2014,
+        "country": "New Zeland",
+        "volume_ml": 330,
+        "alcohol_percentage": 5.2,
+        "sale_price": 145.0
+    } */
 ];
 
