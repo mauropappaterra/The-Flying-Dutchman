3b59ee6e509400415c1550f622a45f49bd37c164
Mauro José Pappaterra <mauropappaterra@gmail.com>
2018-03-21 09:19:23 +0100

    Update DB_SYSTEMBOLAGET.js

--- db/DB_SYSTEMBOLAGET.js
+++ db/DB_SYSTEMBOLAGET.js
@@ -1,6 +1,6 @@
 /** The Flying Dutchman
  *  DB_SYSTEMBOLAGET.js
- *  Created by 'Pirate of the Caribbean' on 17 of February 2018.
+ *  Created by 'Pirates of the Caribbean' on 17 of February 2018.
  */
 
 /*DB of Systembolaget saved as an array of objects, each object represents a beverage*/
