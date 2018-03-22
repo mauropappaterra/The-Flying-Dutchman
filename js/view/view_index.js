5139e5e4a058583830aed06232b447096e01aeca
yiyawen <36305304+yiyawen@users.noreply.github.com>
2018-03-22 15:33:02 +0100

    Update view_index.js

--- js/view/view_index.js
+++ js/view/view_index.js
@@ -22,41 +22,41 @@ var welcome = ["Welcome to The Flying Dutchman!",
                "Välkommen til Flygande Holländaren!",
                "Benvenuti al'Olandese Volante!",
                "Bienvenidos al Holandés Volador!",
-               "Ku soo Dhawow Qaadashada Fly Holland"]
+               "Ahoy t' Th' Flyin' Dutchman!"]
 
 var customer = ["Order your Drinks!",
                 "Logga in som gäst",
                 "Accesso avventore",
                 "Acceso cliente",
-                "Ahoy Buccaneers"]
+                "Order yer Drinks"]
 
 var bartender = ["Bartender login",
                  "Logga in som bartender",
                  "Accesso barista",
                  "Acceso barista",
-                 "Bartender galaan"]
+                 "Bartender login"]
 
 var management = ["Management login",
                   "Logga in som arbetledare",
                   "Accesso gestione",
                   "Acceso administración",
-                  "Goobta maamulka"]
+                  "Management login"]
 
 var user_name = ["User name",
                  "Användarnamn",
                  "Nome utente",
                  "Nombre de usuario",
-                 "Magaca isticmaalaha"]
+                 "User name"]
 
 var password = ["Password",
                 "Lösenord",
                 "Parola d'ordine",
                 "Contraseña",
-                "Furaha"]
+                "Password"]
 
 var login = ["Login",
              "Logga in",
              "Accesso",
              "Iniciar sesión",
-             "Galitaanka"]
+             "Login"]
 
