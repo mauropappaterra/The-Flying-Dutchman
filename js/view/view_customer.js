27a5e6e2edd4989d18e5215e9c6343b76621aa5e
Mauro José Pappaterra <mauropappaterra@gmail.com>
2018-03-22 11:47:29 +0100

    added translation for pop up messages

--- js/view/view_customer.js
+++ js/view/view_customer.js
@@ -23,135 +23,119 @@ var title = ["The Flying Dutchman's List of Drinks",
     "Flygande Holländaren drycklista",
     "Lista di bevande nell'Olandese Volante",
     "Lista de tragos del Holandés Volador",
-    "Noocyada Taxanaha Leh ee Holland"]
+    "TODO: USE OLD"];
 
 // User info panel (top right corner)
 var customer= ["Customer:",
-"kund:",
-"cliente:",
-"cliente:",
-"Macaamiisha:"
-]
+"Kund:",
+"Cliente:",
+"Cliente:",
+"Pirate:"
+];
 
 var credit= ["Credit:",
 "Kreditera:",
 "Credito:",
 "Crédito:",
-"Amaah:"
-]
+"Yu'r Loot:"
+];
 
 // The drink list panel
 var drink_list= ["Our Drink List",
 "Vår drycklista",
 "La nostra lista delle bevande",
 "Nuestra lista de bebidas",
-"Liiskayaga cabitaanka"
-]
+"Booty Chest"
+];
 // start drinks taps
 var all= ["All",
 "Allt",
-"Tutti",
+"Tutte",
 "Todas",
-"Dhammaan"
-]
+"Argh!"
+];
 
 var beers= ["Beers",
 "Beers",
 "Birre",
 "Cervezas",
-"Beers"
-]
+"Them Beers"
+];
 
 var wines= ["Wines",
 "Vin",
 "Vini",
 "Vinos",
-"Wines"
-]
+"Ol' Wines"
+];
 
 var spiritis= ["Spirits",
 "Sprit",
 "Sprits",
 "Bebida Espirituosa",
-"Suufiyada"
-]
+"Rons!"
+];
 
 var kosher= ["Kosher",
 "Koscher",
 "Kosher",
 "Kosher",
-"Kosher"
-]
+"K'sher"
+];
 
 var ecologic= ["Ecologic",
 "Ekologisk",
 "Ecologico",
 "Ecológico",
-"Ecologic"
-]
+"'Cologic"
+];
 
 var specials= ["specials",
 "Särskild",
 "Speciali",
 "Especiales",
-"khaas ah"
-]
+"King' Onlies"
+];
 //end of drinks taps
 
 var your_order= ["Your Order",
 "Du beställer",
 "Il tuo ordine",
 "Su pedido",
-"Amarkaaga"
-]
+"Th' Booty"
+];
 
 var total= ["TOTAL:",
 "TOTAL:",
 "TOTALE:",
 "TOTAL:",
 "WADARTA:"
-]
+];
 
 var pl_order= ["Place Order",
 "Beställa",
 "Invia ordine",
 "Realizar pedido",
-"Amarka Goobta"
-]
+"Board 'hem!"
+];
 
-var hello = ["Hello ",
-             "Hejsan ",
-             "Ciao ",
-             "Hola ",
-             "Ahoy "]
+var popUp_error = ["You must select your drinks before placing an order!",
+    "TODO swedish",
+    "TODO italian",
+    "TODO spanish",
+    "TODO pirate"
+];
 
-var your_credit = ["Your credit is ",
-                   "Din kredit är ",
-                   "Il tuo credito è ",
-                   "Tu crédito es ",
-                   "In yer duffle there be " ]
+var popUp_vip = ["Your order has been payed using your available credit, you can pick up your beverages in the VIP fridge!",
+    "TODO swedish",
+    "TODO italian",
+    "TODO spanish",
+    "TODO pirate"
+];
 
-var empty_order = ["You must select your drinks before placing an order!",
-                   "Du måste välja dina drycker innan du gör din beställning!",
-                   "È necessario selezionare le bevande prima di effettuare un ordine!",
-                   "¡Debe seleccionar sus bebidas antes de realizar un pedido!",
-                   "Ye must select yer drinks afore placin' an order!"
-                  ]
-
-var payed_order = ["Your order has been payed using your available credit, you can pick up your beverages in the VIP fridge.",
-                   "Din beställning har blivit betalad med din tillgängliga kredit, du kan hämta dina drycker i VIP kylen.",
-                   "Il tuo ordine è stato pagato utilizzando il tuo credito disponibile, puoi ritirare le tue bevande nel frigo VIP.",
-                   "Su orden ha sido pagada usando su crédito disponible, puede recoger sus bebidas en el refrigerador VIP.",
-                   "Yer order has been payed usin' yer available credit, ye can pick up yer beverages in th' VIP fridge."
-                  ]
-
-var placed_order = ["Your order has been placed! Direct to the counter and pay for your order before you can pick up your drinks.",
-                    "Din beställning är skickad! Var vänlig betala vid disken innan du kan hämta dina drycker.",
-                    "Il tuo ordine è stato inoltrato! Direttamente al banco e paga il tuo ordine prima di poter ritirare i tuoi drink.",
-                    "¡Su orden ha sido puesta! Dirígete al mostrador y paga tu pedido antes de que puedas recoger tus bebidas.",
-                    "Yer order has been placed! Direct t' th' counter 'n pay fer yer order afore ye can pick up yer drinks."         
-                 ] 
-
-var empty_order_msg = empty_order[0]
-var payed_order_msg = payed_order[0]
-var placed_order_msg = placed_order[0]
+var popUp_payment = ["Your order has been placed!, Direct to the counter and pay for your order before you can pick up your drinks!",
+    "TODO swedish",
+    "TODO italian",
+    "TODO spanish",
+    "TODO pirate"
+];
