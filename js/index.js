3369218a0ddabd5604ebd30dae496dccd345bde9
Mauro José Pappaterra <mauropappaterra@gmail.com>
2018-03-22 11:34:29 +0100

    added language index to local storage, pushed translate function to the bottom

--- js/index.js
+++ js/index.js
@@ -8,15 +8,8 @@
 * Methods translate() and responsive() are unique for each individual page.
 */
 
-function translate (index) {
-    $("#page_title").text(page_title[index]);
-    $("#welcome").text(welcome[index]);
-    $("#customer").text(customer[index]);
-    $("#bartender").text(bartender[index]);
-    $("#management").text(management[index]);
-    $("#user_name").text(user_name[index]);
-    $("#password").text(password[index]);
-    $("#login").text(login[index]);
+if (localStorage.getItem("index") == null){
+    localStorage.setItem("index",0)
 }
 
 function responsive() {
@@ -93,4 +86,14 @@ function drop(ev) {
     alert(data);
     document.getElementById(ev).style.display = "none";
 }
-;
+
+function translate (index) {
+    $("#page_title").text(page_title[index]);
+    $("#welcome").text(welcome[index]);
+    $("#customer").text(customer[index]);
+    $("#bartender").text(bartender[index]);
+    $("#management").text(management[index]);
+    $("#user_name").text(user_name[index]);
+    $("#password").text(password[index]);
+    $("#login").text(login[index]);
+}
