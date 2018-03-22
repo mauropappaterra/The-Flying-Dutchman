d50d6041c1ee42c194cafd57805c2648bcc5fa80
yaila <jenny_4o@hotmail.com>
2018-03-22 10:35:05 +0100

    reinstated sudo user

--- js/login.js
+++ js/login.js
@@ -5,8 +5,7 @@
 
 var user = "blank";
 
-function findByID(id, db) { // assuming 'id' is the first objectvalue
-    
+function findByID(id, db) { // assuming 'id' is the first objectvalue    
     var new_user;
     $.each(db, function(element) {
         var found = false;
@@ -22,11 +21,9 @@ function typeToDB(usertype) {
     if (usertype == 'customer')   { return DB_CUSTOMERS;}
     if (usertype == 'bartender')  { return DB_BARTENDERS;}
     if (usertype == 'management') { return DB_MANAGERS;}
-
-   // else {alert("todo?: invalid userType")}
 }
 
-
+// direct the user to correct page on login
 function goToUserPage(usertype) {
     if      (usertype == 'customer')   { window.location.href = "customer.html";}
     else if (usertype == 'bartender')  { window.location.href = "bartender.html";}
@@ -34,6 +31,7 @@ function goToUserPage(usertype) {
     else { window.location.href = "index.html";}
 }
 
+// login the user if they have valid credentials
 function loginDB(form) {
     var username = form.user_name.value;
     var password = form.password.value;
@@ -51,16 +49,15 @@ function loginDB(form) {
             return false;
         }
         if (index + 1 == db.length) {alert("Wrong username");}
-    });
-    
-
+    });   
 }
 
-function checkAccess() {
-    
+// check if the currently logged in user have the proper level of access to use the current page
+function checkAccess() {    
     var ut = localStorage.getItem('usertype');
     var page = window.location.pathname.split("/").pop().split(".")[0];
-    if ((ut == page) || (ut == null && page == 'index') || page == 'index' || localStorage.getItem("id").length < 2 ) {return;}
+    var id = (localStorage.getItem("id")).slice(1, localStorage.getItem("id").length);
+    if ((ut == page) || (ut == null && page == 'index') || page == 'index' || id == "€$#=@") {return;}
     else {
         alert("Acces Denied");
         goToUserPage(ut);
@@ -76,6 +73,7 @@ function isVIP(user) {
     return user.vip;
 }
 
+// onload, check the users access and load the users name etc for display
 $(function () {
     checkAccess();
     user = findByID(localStorage.getItem("id"), typeToDB(localStorage.getItem("usertype")));
