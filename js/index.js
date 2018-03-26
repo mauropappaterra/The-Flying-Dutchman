6fd710038373cbe97fc7ca90ec5a89b94d3184c2
yaila <jenny_4o@hotmail.com>
2018-03-26 13:06:55 +0200

    index reset background fix

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ function picToGif(ev) {
         $('#great').hide();
         document.getElementById('great').pause();
         $('body').css('background-color', "white");
-        $('body').css('background-image', 'url("img/background/back12.png")');
+        $('body').css('background-image', 'url(img/storm.jpg)');
     }
 }
 
