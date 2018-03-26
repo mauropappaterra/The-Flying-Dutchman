6ece99f4eef64fb470548ff57f04576d4d3321d2
yaila <jenny_4o@hotmail.com>
2018-03-26 21:01:25 +0200

    updated tab highlighting

--- js/bartender.js
+++ js/bartender.js
@@ -377,9 +377,12 @@ function highlightTab(new_tab) {
     // reset previous tab display
     previous_tab = '*#' + current_tab;
     $(previous_tab).css("background", "");
+    $(previous_tab).css("border-color", "#567973");
     $(previous_tab + 's').css("background", "");   // ugly solution...
+    $(previous_tab + 's').css("border-color", "#567973"); 
     // highlight current tab
-    $(new_tab).css("background", "#A4B9B6");    
+    $(new_tab).css("background", "#A4B9B6");
+    $(new_tab).css("border-color", "#A4B9B6");
 }
 
 function updateTransactions() { // update the current transactions, local storage
--- js/management.js
+++ js/management.js
@@ -37,7 +37,7 @@ var big_quantity = []; // keeps list of quantities with matching indexes
 var big_total = 0; // calculates the big_total
 
 var current_manager = localStorage.getItem('id');
-var current_tab = "#drinks_list";
+var current_tab = "drinks_list";
 
 /*UNDO-REDO ARRAYS*/
 var done = new Array([]);    //keeps track of 'done' actions
@@ -47,17 +47,17 @@ $(document).ready(function() {
 
     //addBackground();
     retrieveSystembolaget(); // load database on page load
-    highlightTab("#drinks_list");
+    highlightTab("drinks_list");
     
     $("#drinks_list").click(function(){ /* Retrieve from Systembolaget*/
-        highlightTab("#drinks_list");
+        highlightTab("drinks_list");
         $("#drink_database").empty();
         //addBackground();
         retrieveSystembolaget();
     });
 
     $("#specials_db").click(function(){ /* Retrieve from Mike's Specials*/
-        highlightTab("#specials_db");
+        highlightTab("specials_db");
         $("#drink_database").empty();
         //addBackground();
         $.each(DB_MIKES, function(element){
@@ -66,7 +66,7 @@ $(document).ready(function() {
     });
 
     $("#check_stock").click(function(){ /* Retrieve in stock*/
-        highlightTab("#check_stock");
+        highlightTab("check_stock");
         $("#drink_database").empty();
         //addBackground();
         $.each(DB_SYSTEMBOLAGET, function(element){
@@ -77,7 +77,7 @@ $(document).ready(function() {
     });
 
     $("#shortage_alert").click(function(){ /* Retrieve on shortage alert*/
-        highlightTab("#shortage_alert");
+        highlightTab("shortage_alert");
         $("#drink_database").empty();
         //addBackground();
         $.each(DB_SYSTEMBOLAGET, function(element){
@@ -448,9 +448,12 @@ function resetPage() {
 // highlight the currently displayed tab 
 function highlightTab(new_tab) {
     // reset previous tab display
-    $(current_tab).css("background", "");   
+    previous_tab = '#' + current_tab;
+    $(previous_tab).css("background", "");
+    $(previous_tab).css("border-color", "#567973");
     // highlight current tab
-    $(new_tab).css("background", "#A4B9B6");
+    $('#' + new_tab).css("background", "#A4B9B6");
+    $('#' + new_tab).css("border-color", "#A4B9B6");
     current_tab = new_tab;
 }
 
